daf4f4395c6567274462b32e10d2b40e4f5d63e8
Votre Nom <votre_email@exemple.com>
2025-04-12 07:03:10 +0200

    add debuggercmd

--- nvim/.config/nvim/lua/config/debuger_keymaps.lua
+++ nvim/.config/nvim/lua/config/debuger_keymaps.lua
@@ -1,14 +1,18 @@
 local dap = require("dap")
 local dapui = require("dapui")
 
--- Points d'arrêt
-vim.keymap.set("n", "<leader>db", function() dap.toggle_breakpoint() end, { desc = "Toggle Breakpoint" })
+
+-- Étapes dans le code
+vim.keymap.set("n", "<F10>", function() dap.step_over() end, { desc = "Step Over" })
+vim.keymap.set("n", "<F11>", function() dap.step_into() end, { desc = "Step Into" })
+vim.keymap.set("n", "<F12>", function() dap.step_out() end, { desc = "Step Out" })
 
 -- Contrôle du débogueur
 vim.keymap.set("n", "<leader>dc", function() dap.continue() end, { desc = "Continue Debugging" })
-vim.keymap.set("n", "<leader>ds", function() dap.step_over() end, { desc = "Step Over" })
-vim.keymap.set("n", "<leader>di", function() dap.step_into() end, { desc = "Step Into" })
-vim.keymap.set("n", "<leader>do", function() dap.step_out() end, { desc = "Step Out" })
+vim.keymap.set("n", "<leader>dT", function() dap.terminate() end, { desc = "Terminate Debugging" })
+
+-- Points d'arrêt
+vim.keymap.set("n", "<leader>db", function() dap.toggle_breakpoint() end, { desc = "Toggle Breakpoint" })
 
 -- Interface utilisateur DAP UI
 vim.keymap.set("n", "<leader>du", function() dapui.toggle() end, { desc = "Toggle DAP UI" })
