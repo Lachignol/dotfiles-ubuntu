505a6f9f1790af03e3fbe589129389f818025c3a
Votre Nom <votre_email@exemple.com>
2025-04-12 05:53:56 +0200

    mynvim-fixdepreciate

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -52,3 +52,8 @@ vim.api.nvim_set_keymap("i", "<Up>", "", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("i", "<Down>", "", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("i", "<Left>", "", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("i", "<Right>", "", { noremap = true, silent = true })
+
+-------Mapper Ctrl-S pour enregistrer le buffer---------------------------------------
+vim.keymap.set("n", "<C-s>", ":w<CR>", { noremap = true, silent = true }) -- Mode normal
+vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>a", { noremap = true, silent = true }) -- Mode insertion
+
