dd7a4244068843576aef9432a69ad92af1449360
Votre Nom <votre_email@exemple.com>
2025-04-15 04:35:13 +0200

    bear experimentation and fix false positive header not used issue

--- nvim/.config/nvim/lua/config/lsp_config.lua
+++ nvim/.config/nvim/lua/config/lsp_config.lua
@@ -92,9 +92,29 @@ lspconfig.pyright.setup({
 })
 
 -- clangd (C/C++)
-require("lspconfig").clangd.setup {
-  cmd = { "clangd", "--background-index", "--header-insertion=never" },
-}
+lspconfig.clangd.setup({
+  cmd = { "clangd", "--background-index","--header-insertion=never" },
+  on_attach = function(client, bufnr)
+    vim.lsp.handlers["textDocument/publishDiagnostics"] = function(_, result, ctx, config)
+      if not result or not result.diagnostics then return end
+
+      local filtered_diagnostics = {}
+      for _, diagnostic in ipairs(result.diagnostics) do
+        if not diagnostic.message:find("Included header") then
+          table.insert(filtered_diagnostics, diagnostic)
+        end
+      end
+
+      result.diagnostics = filtered_diagnostics
+      vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
+    end
+  end,
+})
+
+-- lspconfig.clangd.setup({
+  -- cmd = { "clangd", "--header-insertion=never" },
+-- })
+
 -- tsserver (TypeScript/JavaScript)
 lspconfig.ts_ls.setup({
   on_attach = on_attach,
