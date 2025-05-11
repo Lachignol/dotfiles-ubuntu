f67f05928de16ce09820e1a75c9af9f7291f8183
Votre Nom <votre_email@exemple.com>
2025-05-11 18:33:03 +0200

    aliasesupdate

--- zsh/.config/zsh/aliases/aliases.zsh
+++ zsh/.config/zsh/aliases/aliases.zsh
@@ -1,10 +1,2 @@
-<<<<<<< Updated upstream
-=======
-alias nvim-scratch='XDG_CONFIG_HOME=~/.config nvim -u ~/.config/nvim-scratch/init.lua'
-# Alias pour la configuration alternative "nvim-scratch"
-alias my-nvim='NVIM_APPNAME="nvim-scratch" nvim'
-# Alias pour la configuration principale de Neovim
-alias nvim='NVIM_APPNAME="nvim" nvim'
->>>>>>> Stashed changes
 alias compile="cc -Wall -Wextra -Werror *.c -o diamond && ./diamond"
 alias config-zsh="vim ~/.zshrc"
